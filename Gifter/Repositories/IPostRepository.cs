901f50a822e50d97255a40cf5d4274514bc10328
cslunetta <cslunetta@outlook.com>
2021-04-13 13:57:57 -0500

    added search endpoint to api

--- Gifter/Repositories/IPostRepository.cs
+++ Gifter/Repositories/IPostRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Gifter.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Gifter.Repositories
@@ -12,5 +13,7 @@ namespace Gifter.Repositories
         Post GetById(int id);
         void Update(Post post);
         Post GetPostByIdWithComments(int id);
+        List<Post> Search(string criterion, bool sortDescending);
+        List<Post> Hottest(DateTime dateTime, bool sortDescending);
     }
 }
